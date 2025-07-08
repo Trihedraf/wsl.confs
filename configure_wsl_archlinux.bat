d2460fca7411f6d768f54a4c6a67439edfba26e5
Trihedraf <forrest@mailfsh.com>
2025-07-08 15:41:58 -0700

    Create configure_wsl_archlinux.bat

---
+++ configure_wsl_archlinux.bat
@@ -0,0 +1,5 @@
+wsl --manage archlinux --set-sparse true --allow-unsafe
+wsl -d archlinux pacman -Syyu --noconfirm
+type .\etc\pacman.conf | CLIP && wsl sh -c "powershell.exe Get-Clipboard > /etc/pacman.conf"
+wsl -d archlinux sh -c "useradd -m -G wheel <USER> && passwd <USER> && echo -e '[user]\ndefault=<USER>' >> /etc/wsl.conf"
+wsl --terminate archlinux
