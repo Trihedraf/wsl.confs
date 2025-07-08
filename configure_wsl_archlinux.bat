e6b96898ec06adf45f9d7dfb4993daa2097c0e7a
Trihedraf <forrest@mailfsh.com>
2025-07-08 15:51:14 -0700

    add installing necessary packages

--- configure_wsl_archlinux.bat
+++ configure_wsl_archlinux.bat
@@ -1,5 +1,6 @@
 wsl --manage archlinux --set-sparse true --allow-unsafe
 wsl -d archlinux pacman -Syyu --noconfirm
 type .\etc\pacman.conf | CLIP && wsl sh -c "powershell.exe Get-Clipboard > /etc/pacman.conf"
+wsl -d archlinux sh -c "pacman -S git micro nano sudo superfile wget"
 wsl -d archlinux sh -c "useradd -m -G wheel <USER> && passwd <USER> && echo -e '[user]\ndefault=<USER>' >> /etc/wsl.conf"
 wsl --terminate archlinux
