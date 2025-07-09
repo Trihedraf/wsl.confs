8d7ace8396784f8da47c0346e83d3c6046cee2ec
Trihedraf <forrest@mailfsh.com>
2025-07-09 14:19:35 -0700

    Set sudo permssions for group wheel

--- configure_wsl_archlinux.bat
+++ configure_wsl_archlinux.bat
@@ -1,5 +1,6 @@
 wsl --shutdown && wsl --manage archlinux --set-sparse true --allow-unsafe
 type .\etc\pacman.conf | CLIP && wsl sh -c "powershell.exe Get-Clipboard > /etc/pacman.conf"
 wsl -d archlinux sh -c "pacman -S git micro nano sudo superfile wget"
+type .\etc\sudoers.d\wheel | CLIP && wsl sh -c "powershell.exe Get-Clipboard > /etc/sudoers.d/wheel"
 wsl -d archlinux sh -c "useradd -m -G wheel <USER> && passwd <USER> && echo -e '[user]\ndefault=<USER>' >> /etc/wsl.conf"
 wsl --terminate archlinux
