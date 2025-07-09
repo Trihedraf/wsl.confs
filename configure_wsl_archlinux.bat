b709d1a562558272e96c145dff423f0a260e073f
Trihedraf <forrest@mailfsh.com>
2025-07-09 14:20:12 -0700

    limit to only necessary packages

--- configure_wsl_archlinux.bat
+++ configure_wsl_archlinux.bat
@@ -1,6 +1,6 @@
 wsl --shutdown && wsl --manage archlinux --set-sparse true --allow-unsafe
 type .\etc\pacman.conf | CLIP && wsl sh -c "powershell.exe Get-Clipboard > /etc/pacman.conf"
-wsl -d archlinux sh -c "pacman -S git micro nano sudo superfile wget"
+wsl -d archlinux pacman -Syyu --noconfirm git sudo
 type .\etc\sudoers.d\wheel | CLIP && wsl sh -c "powershell.exe Get-Clipboard > /etc/sudoers.d/wheel"
 wsl -d archlinux sh -c "useradd -m -G wheel <USER> && passwd <USER> && echo -e '[user]\ndefault=<USER>' >> /etc/wsl.conf"
 wsl --terminate archlinux
