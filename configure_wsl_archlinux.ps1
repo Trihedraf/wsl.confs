65bea41f97cf8f719190745d3019457b06911c0d
Trihedraf <forrest@mailfsh.com>
2025-07-10 06:31:49 -0700

    update configure_wsl_archlinux.ps1

--- configure_wsl_archlinux.ps1
+++ configure_wsl_archlinux.ps1
@@ -1,6 +1,7 @@
 wsl --shutdown && wsl --manage archlinux --set-sparse true --allow-unsafe
-type .\etc\pacman.conf | CLIP && wsl sh -c "powershell.exe Get-Clipboard > /etc/pacman.conf"
-wsl -d archlinux pacman -Syyu --noconfirm git sudo
-type .\etc\sudoers.d\wheel | CLIP && wsl sh -c "powershell.exe Get-Clipboard > /etc/sudoers.d/wheel"
-wsl -d archlinux sh -c "useradd -m -G wheel <USER> && passwd <USER> && echo -e '[user]\ndefault=<USER>' >> /etc/wsl.conf"
+wsl -d archlinux sh -c 'printf "en_US.UTF-8 UTF-8\n" >> /etc/locale.gen && locale-gen'
+Get-Content .\etc\pacman.conf | CLIP && wsl sh -c 'powershell.exe Get-Clipboard > /etc/pacman.conf'
+wsl -d archlinux pacman -Syyu --noconfirm sudo
+Get-Content .\etc\sudoers.d\wheel | CLIP && wsl sh -c 'powershell.exe Get-Clipboard > /etc/sudoers.d/wheel'
+wsl -d archlinux sh -c 'useradd -m -G wheel <USER> && passwd <USER> && printf "[user]\ndefault=<USER>\n" >> /etc/wsl.conf'
 wsl --terminate archlinux
