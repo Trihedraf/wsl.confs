90739eae7fca9e87c0a8c59b34de9c64f8962269
Trihedraf <forrest@mailfsh.com>
2025-07-11 17:20:51 -0700

    replace sudo with base-devel

--- configure_wsl_archlinux.ps1
+++ configure_wsl_archlinux.ps1
@@ -1,6 +1,6 @@
 wsl --shutdown && wsl --manage archlinux --set-sparse true --allow-unsafe
 wsl -d archlinux sh -c 'printf "en_US.UTF-8 UTF-8\n" >> /etc/locale.gen && locale-gen'
-wsl -d archlinux pacman -Syyu --noconfirm sudo
+wsl -d archlinux pacman -Syyu --noconfirm --needed base-devel
 Get-Content .\etc\sudoers.d\wheel | CLIP && wsl sh -c 'powershell.exe Get-Clipboard > /etc/sudoers.d/wheel'
 wsl -d archlinux sh -c 'useradd -m -G wheel <USER> && passwd <USER> && printf "[user]\ndefault=<USER>\n" >> /etc/wsl.conf'
 wsl --terminate archlinux
